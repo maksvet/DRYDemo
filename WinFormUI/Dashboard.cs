7592fda875d373535b793f2ec37283bc1b03f1ce
Max Svetlakov <maksim.svetlakov@focisoulutions.com>
2022-03-07 16:08:34 -0500

    Add project files.

---
+++ WinFormUI/Dashboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormUI
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void generateEmployeeIdButton_Click(object sender, EventArgs e)
+        {
+            employeeIdText.Text = GenerateEmployeeID(firstNameText.Text, lastNameText.Text);
+        }
+        private string GenerateEmployeeID(string firstName, string lastName)
+        {
+            string employeeId = $@"{ firstName.Substring(0, 4) }{ lastName.Substring(0, 4) }{ DateTime.Now.Millisecond }";
+            return employeeId;
+        }
+    }
+}
